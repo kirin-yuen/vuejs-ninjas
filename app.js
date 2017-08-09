88fb0bda52931e00093a9567d175a9e2449371db
kirin-yuen <ywl_allen@163.com>
2017-08-09 09:05:27 +0800

    vuejs event

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ var vue = new Vue({
     // which tag is going to be take over by vue
     el: '#app',
     data: {
-        name: 'kirin',
-        favourite: 'aikido',
-        website: 'http://www.sina.com.cn',
-        template: '<a href="http://www.sina.com.cn">another sina</a>'
+        age: 30,
+        x: 0,
+        y: 0
     },
     methods: {
-        greet: function(str){
-            // vuejs takes all of these differents properties we have in the data object
-            // and proxy them to the top
-            return 'good ' + str + ' ' + this.name;
+        subtract: function(num){
+            this.age -= num;
+        },
+        // first param will be event if no parma pass
+        updateXY: function(event){
+            this.x = event.clientX;
+            this.y = event.clientY;
         }
     }
 });
