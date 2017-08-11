e5d7c8ef6a1b8ab7999f287bc3ec63ed768d193c
kirin-yuen <ywl_allen@163.com>
2017-08-11 17:36:39 +0800

    computed properties

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ var vue = new Vue({
     // which tag is going to be take over by vue
     el: '#app',
     data: {
-        myName: '',
-        yourName: '',
-        hisName: ''
+        num: 0,
+        a: 0,
+        b: 0
     },
     methods: {
-        update: function(e) {
-            this.myName = e.target.value;
+        addToA: function() {
+            console.log('fire addToA()');
+            return this.a + this.num;
+        },
+        addToB: function() {
+            console.log('fire addToB()');
+            return this.b + this.num;
+        }
+    },
+    computed: {
+        resultA: function() {
+            console.log('fire resultA');
+            return this.a + this.num;
+        },
+        resultB: function() {
+            console.log('fire resultB');
+            return this.b + this.num;
         }
     }
 });
