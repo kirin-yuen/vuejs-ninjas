7d26ae93a567d85289f1d5ef12648bb1ff2b44e4
kirin-yuen <ywl_allen@163.com>
2017-08-11 17:58:08 +0800

    dynamic CSS classes

--- app.js
+++ app.js
@@ -2,28 +2,15 @@ var vue = new Vue({
     // which tag is going to be take over by vue
     el: '#app',
     data: {
-        num: 0,
-        a: 0,
-        b: 0
-    },
-    methods: {
-        addToA: function() {
-            console.log('fire addToA()');
-            return this.a + this.num;
-        },
-        addToB: function() {
-            console.log('fire addToB()');
-            return this.b + this.num;
-        }
+        nearby: false,
+        available: false
     },
     computed: {
-        resultA: function() {
-            console.log('fire resultA');
-            return this.a + this.num;
-        },
-        resultB: function() {
-            console.log('fire resultB');
-            return this.b + this.num;
+        compClasses: function() {          
+            return {
+                nearby: this.nearby,
+                available: this.available
+            }
         }
     }
 });
