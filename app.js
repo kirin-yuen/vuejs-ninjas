c4593f64ee83abdad30d770b86b5afccd5a28079
kirin-yuen <ywl_allen@163.com>
2017-08-14 17:40:39 +0800

    conditional

--- app.js
+++ app.js
@@ -2,15 +2,7 @@ var vue = new Vue({
     // which tag is going to be take over by vue
     el: '#app',
     data: {
-        nearby: false,
-        available: false
-    },
-    computed: {
-        compClasses: function() {          
-            return {
-                nearby: this.nearby,
-                available: this.available
-            }
-        }
+        error: false,
+        success: false
     }
 });
