3294fa0802e61db1cb098ef96d547256a60f45c9
kirin-yuen <ywl_allen@163.com>
2017-09-07 17:43:43 +0800

    refs

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
-// golbal register component before initialize vue instance
-Vue.component('conponent-global', {
-    template: '<p>{{title}}</p>',
-    // conponent data should be a function and return a fresh object to avoid sharing data
-    data: function(){
-        return {
-            title: 'Vue global component'
-        }
-    }
-});
-
-var a = new Vue({
+new Vue({
     el: '#app',
-    // local register component
-    components: {
-        'conponent-local': {
-            template: '<p>{{title}}</p>',
-            data: function(){
-                return {
-                    title: 'Vue local component'
-                }
-            }
+    data:{
+        output: 'no data'
+    },
+    methods: {
+        update: function(target) {
+            // use refs to update our data
+            this.output = this.$refs[target].value;
         }
     }
 });
