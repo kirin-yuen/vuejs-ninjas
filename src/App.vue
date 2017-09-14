2d81b530a31286307e96fd8ca3e903d4d0911012
kirin-yuen <ywl_allen@163.com>
2017-09-14 09:10:20 +0800

    scope css

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
-    <!-- 
-        components: we can code it once and nest it to other components for serval times if need. For large application split our code to different components, we can nest those component in the root component
-     -->
     <div>
         <h1>{{title}}</h1>
-        <!-- globally register in main.js via Vue.component() -->
-        <global-conponent></global-conponent>
-        <local-conponent></local-conponent>
+        <Ninja></Ninja>
     </div>
 </template>
 <script>
@@ -14,14 +9,21 @@ export default {
     // name: 'app',
     data() {
         return {
-            title: 'Nesting components'
+            title: 'Scope css'
         }
     },
     // locally register via components
     components:{
-        'local-conponent': require('./Local-component.vue')
+        Ninja: require('./Ninja.vue')
     }
 }
 </script>
+<!-- no scoped attr will affect globally -->
 <style>
+h1 {
+    background: red;
+}
+p {
+    color :green;
+}
 </style>
