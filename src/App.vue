1a0535e9a07e5072ccda69d5e60b1ffdd878cdbc
kirin-yuen <ywl_allen@163.com>
2017-10-02 12:19:44 +0800

    input binding

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,8 @@
 <template>
     <div>
         <h1>{{title}}</h1>
-        <hr>
+        <my-form></my-form>
 
-        <h2>switch component via v-show</h2>
-        <formOne v-show="flag"></formOne>
-        <formTwo v-show="!flag"></formTwo>
-        <button @click="flag = !flag">switch</button>
-
-        <h2>switch component via components tag</h2>
-        <!-- switch will create a new instance and go through lifecycle again via component-->
-        <!-- we can use keep-alive to retain the data -->
-        <keep-alive>            
-            <component :is="shownComponet"></component>
-        </keep-alive>
-        <select v-model="shownComponet">
-            <option value="formOne">login form</option>
-            <option value="formTwo">register form</option>
-        </select>
     </div>
 </template>
 <script>
@@ -25,15 +10,12 @@ export default {
     // name: 'app',
     data() {
         return {
-            title: 'dynamic component',
-            flag: true,
-            shownComponet: 'formOne'
+            title: 'input binding'
         }
     },
     // locally register via components
     components:{
-        formOne: require('./form-one.vue'),
-        formTwo: require('./form-two.vue')
+        'my-form': require('./form.vue')
     }
 }
 </script>
---
+++ src/form.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>v-model</h1>
+        <label for="">
+            Some text here
+            <input type="text" v-model="text">
+        </label>
+        <p>Preview: {{text}}</p>
+        <hr>
+
+        <h1>Syntactic sugar of v-model</h1>
+        <label for="">
+            Some text here
+            <!-- @change or @blur would be sugar of v-model.lazy -->
+            <input type="text" :value="text1" @input="text1 = $event.target.value">
+        </label>
+        <p>Preview: {{text1}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+  name: 'form',
+
+  data () {
+    return {
+        text: '',
+        text1: ''
+    };
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
